c13a5a584d0135b5fbc40fac94a200442dd136cc
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-03-01 13:38:14 -0300

    Add files via upload
    refactoring the queries files for the different entities

---
+++ backend/models/vicionando_series_DB_photos.sql
@@ -0,0 +1,10 @@
+
+use ruscicacode_vicionando_series_2; 
+
+INSERT INTO photos (`filename`, `description`, `filesize`) VALUES
+	('PHOTO20230211172453.jpg', 'Mi foto de perfíl', 1024);
+
+SELECT * FROM photos WHERE id = '1';
+
+
+
