3e16a6361a2c1093bd828b551eacf75c81e532aa
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-03-01 13:39:58 -0300

    Delete vicionando_series_DB_selects.sql
    refactoring

--- backend/models/vicionando_series_DB_selects.sql
+++
@@ -1,6 +0,0 @@
-use ruscicacode_vicionando_series_2; 
-SELECT * FROM users;
-SELECT * FROM photos;
-SELECT * FROM vicionados;
-
--- SELECT id_recuperacion FROM recuperaciones_correo WHERE usuario_id = "5";
