fa7a8d1c21d6005bd7cd924f8db0edf608d8370e
hernanruscica <cesarhernanruscica@gmail.com>
2023-08-05 12:40:00 -0300

    documentacion y orden de la API

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const bodyParser = require('body-parser');
 const app = express();
 
 /*for the index, with some documentation*/
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'ejs');
-
+app.set('hostName', null);
 
 const indexRouter = require('./app/routes/indexRoutes');
 const usersRouter = require('./app/routes/usersRoutes');
 const trackedShowsRouter = require('./app/routes/trackedShowsRoutes');
+
+
 
 
 app.use(bodyParser.json());
@@ -20,6 +22,22 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/tracked-shows', trackedShowsRouter);
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static('./public'));
+
+const port = 1000;
+let hostName = null;
+/*
+app.use((req) => {
+    hostName = req.hostname;
+    console.log(hostName)
+    next();
+});
+*/
+app.listen(port, () => console.log(`App escuchando en https://${hostName}:${port}`));
 
 
-app.listen(1000, () => console.log(`App escuchando en http://localhost:1000`));
+  
+  
+  
+  
--- app/routes/indexRoutes.js
+++ app/routes/indexRoutes.js
@@ -4,14 +4,17 @@ var router = express.Router();
 
 //Index Routes
 router.get('/', (req, res) => {
+    /*
     res.send('<h2>API information /</h2>\
               <h3>rutas:<h3>\
               <p>/ root directory\
               <a href="https://vicionando-series-2-api.onrender.com/api/users/id">/api/users/id</a>  get user by id\
               <a href="https://vicionando-series-2-api.onrender.com/api/users">/api/users/id </a> get all users\
               <p>');
+              */
+    res.render('home');
+    console.log(req.headers.host)
 });
-router.post
 
 module.exports = router;
 
